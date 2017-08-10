20c9fa49cf1d293d719e518768fd8816d36d8958
Robert S Lee <sangkyulee@gmail.com>
2017-08-10 10:25:13 -0400

    use png

--- crdb-column-family.R
+++ crdb-column-family.R
@@ -6,5 +6,5 @@ files = list.files(pattern="*.csv")
 DT = do.call(rbind, lapply(files, fread))
 x <- subset(DT, threadName2 =="tblfam"| threadName2=="tblnofam")
 ggplot(x,aes(x=threadName2,y=Latency))+geom_boxplot()+facet_grid (scenario ~ db)
-ggsave("crdb-column-family.pdf")
+ggsave("crdb-column-family.png")
 
